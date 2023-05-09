8ee26b32bd8de44bf6e3da01f93c8bd1842508e8
Jennifer Zaldivar <zaldivar.jennifer@student.greenriver.edu>
2023-05-09 13:49:33 -0400

    Create fat free variables in index.php, display variables in info.html

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Jennifer Zaldivar
  * 5/9/23
  * 328/week6/index.php
- * Controller for week6 project
+ * Controller for week6 project: Templating
  */
 
 //Turn on error reporting
@@ -17,8 +17,28 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define a default route
-$f3->route('GET /', function () {
+$f3->route('GET /', function ($f3) {
     //echo '<h1>Welcome to week6!</h1>';
+
+    //Add data to f3 "hive"
+    $f3->set('color', 'green');
+    $f3->set('food', 'ramen');
+    $f3->set('title', 'Welcome to Week 6: Templating');
+    $f3->set('temp', 67);
+    //Add radius variable
+    $f3->set('radius', 10);
+    //Add array of fruits to the controller
+    $fruits = array('banana', 'apple', 'kiwi');
+    $f3->set('fruits', $fruits);
+    //Add array of vegetables
+    $veggies = array('carrot', 'spinach', 'lettuce');
+    $f3->set('veggies', $veggies);
+    //Define an associative array of cupcake flavors
+    $cupcakes = array('choc-mocha'=>'Chocolate Mocha',
+                        'straw-cheese'=>'Strawberry Cheesecake',
+                        'blue-lemon'=>'Blueberry Lemon');
+    $f3->set('cupcakes', $cupcakes);
+
 
     //Display a view page
     $view = new Template();
